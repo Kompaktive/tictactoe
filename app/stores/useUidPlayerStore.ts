2377b4a0a1a4f55a86510ee82804b9a43794b8aa
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-03-28 22:08:42 +0700

    feat: create player uid store

---
+++ app/stores/useUidPlayerStore.ts
@@ -0,0 +1,26 @@
+/*
+  use to uniquely identify player, in case they left the game
+  and wants to rejoin the game
+*/
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface UidPlayerStore {
+  uid?: string;
+  setUid: (str: string) => void;
+}
+
+const useUidPlayerStore = create<UidPlayerStore>()(
+  persist(
+    (set) => ({
+      uid: undefined,
+      setUid: (str) => set(() => ({ uid: str })),
+    }),
+    {
+      name: "uid-player-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
+
+export default useUidPlayerStore;
