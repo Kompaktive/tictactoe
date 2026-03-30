4c3c27a960016517e9fa7c2ce1aede74b420f897
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-03-30 13:13:27 +0700

    fix: solve react uncontrolled input warning

--- app/stores/useNicknameStore.ts
+++ app/stores/useNicknameStore.ts
@@ -2,14 +2,14 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface NicknameStore {
-  nickname?: string;
+  nickname: string;
   setNickname: (str: string) => void;
 }
 
 const useNicknameStore = create<NicknameStore>()(
   persist(
     (set) => ({
-      nickname: undefined,
+      nickname: "",
       setNickname: (str) => set(() => ({ nickname: str })),
     }),
     { name: "nickname-storage" },
