12edc77ba15eef9cebb181f8bf6d52f0dd3cf122
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-04-01 12:58:12 +0700

    refactor: delete unused generateUniqueRoomCode
    room Id collision is now managed via 'runTransaction'.

--- app/helpers/room.ts
+++ app/helpers/room.ts
@@ -1,20 +1,7 @@
 import { child, get, ref } from "firebase/database";
 import { database } from "~/firebase";
-import { generateRandomCode } from "~/utils/crypto";
 
 export const checkRoomExists = async (code: string): Promise<boolean> => {
   if (!code) return false;
   return (await get(child(ref(database), `rooms/${code}`))).exists();
-};
-
-export const generateUniqueRoomCode = async (): Promise<string> => {
-  const code = generateRandomCode();
-
-  if (await checkRoomExists(code)) {
-    console.log("room already exist! Generating new room code...");
-    return generateUniqueRoomCode();
-  }
-
-  console.log("room code is unique");
-  return code;
 };
