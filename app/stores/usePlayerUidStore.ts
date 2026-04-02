cbf6e304d735dc4148ab7850f17cb50bdeb2bfc8
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-04-02 18:20:43 +0700

    refactor: set uid type to string
    Prevent 'possibly undefined' type err, as it will never be undefined.

--- app/stores/usePlayerUidStore.ts
+++ app/stores/usePlayerUidStore.ts
@@ -6,14 +6,14 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface PlayerUidStore {
-  uid?: string;
+  uid: string;
   setUid: (str: string) => void;
 }
 
 const usePlayerUidStore = create<PlayerUidStore>()(
   persist(
     (set) => ({
-      uid: undefined,
+      uid: "",
       setUid: (str) => set(() => ({ uid: str })),
     }),
     {
