9b0231e27ce5b6bd40da69e974fc542323282389
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-04-27 17:22:55 +0700

    feat: add hydration tracking to nickname store

--- app/stores/useNicknameStore.ts
+++ app/stores/useNicknameStore.ts
@@ -3,16 +3,26 @@ import { persist } from "zustand/middleware";
 
 interface NicknameStore {
   nickname: string;
+  hasHydrated: boolean;
   setNickname: (str: string) => void;
+  setHasHydrated: (val: boolean) => void;
 }
 
 const useNicknameStore = create<NicknameStore>()(
   persist(
     (set) => ({
       nickname: "",
+      hasHydrated: false,
       setNickname: (str) => set(() => ({ nickname: str })),
+      setHasHydrated: (val) => set(() => ({ hasHydrated: val })),
     }),
-    { name: "nickname-storage" },
+    {
+      name: "nickname-storage",
+      partialize: (state) => ({ nickname: state.nickname }),
+      onRehydrateStorage: () => (state) => {
+        if (state) state.setHasHydrated(true);
+      },
+    },
   ),
 );
 
