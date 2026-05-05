ad41a8103ad6ba6ad0a73886bffa97bd48135c8c
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-05-05 13:15:32 +0700

    feat: install rajdhani font

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,17 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
