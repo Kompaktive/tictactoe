697528b8fefe80ce8d851d81833d3718918390e3
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-06-10 16:14:05 +0700

    feat: create icon to decorate main menu

---
+++ app/components/atoms/icons/TicTacToeIcon.tsx
@@ -0,0 +1,26 @@
+import { motion, type HTMLMotionProps } from "motion/react";
+import { cn } from "~/utils/cn";
+import OMarker from "./OMarker";
+import XMarker from "./XMarker";
+
+export const TicTacToeIcon = ({
+  className,
+  ...props
+}: HTMLMotionProps<"div">) => {
+  return (
+    <motion.div className={cn("grid grid-cols-2", className)} {...props}>
+      <div className="border-r border-b">
+        <XMarker className="stroke-accent-1 animate-pulse" />
+      </div>
+      <div>
+        <OMarker className="stroke-accent-2 animate-pulse border-b border-l [animation-delay:1000ms]" />
+      </div>
+      <div>
+        <OMarker className="stroke-accent-2 animate-pulse border-t border-r [animation-delay:500ms]" />
+      </div>
+      <div>
+        <XMarker className="stroke-accent-1 animate-pulse border-t border-l [animation-delay:1500ms]" />
+      </div>
+    </motion.div>
+  );
+};
