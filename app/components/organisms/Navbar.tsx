9a6b7e2699604024172f2cde8d89e166b6d12eea
Kompaktive <alfian.kurniadi.dev@gmail.com>
2026-06-12 17:22:00 +0700

    feat: add home button

--- app/components/organisms/Navbar.tsx
+++ app/components/organisms/Navbar.tsx
@@ -1,6 +1,16 @@
+import { MdHome } from "react-icons/md";
+import { Link } from "react-router";
+
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 right-0 left-0 flex items-center justify-end p-4">
+    <nav className="sticky top-0 right-0 left-0 flex items-center justify-between p-4">
+      <Link
+        to="/"
+        className="hover:text-accent-2 flex items-center gap-2 transition duration-150 hover:cursor-pointer"
+      >
+        <MdHome size={20} />
+        Back to Main Menu
+      </Link>
       <div>Github</div>
     </nav>
   );
